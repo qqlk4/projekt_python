98341d104f92e3895992e67384599eceac59c47a
qqlk4 <kwnek472@gmail.com>
2024-06-05 20:51:52 +0200

    map maker

---
+++ MapMaker.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+
+
+def map_maker(date_in_mm):
+
+    # zaczytanie bazy danych lotnisk i lotnisk docelowych w dniu wylotu (bez tych 3 dni do przodu)
+    database = pd.read_csv('temp_logs/iata-icao.csv')
+    dane = pd.read_csv('temp_logs/flights_data.csv')
+    dane = dane[dane["data"] == date_in_mm]
+
+    filtered_airports = database[database['iata'].isin(dane['miasto'])]
+
+    # wyznaczanie granic mapy na podstawie minimalnych i maksymalnych wartości szerokości i długości geograficznej
+    min_lon = filtered_airports['longitude'].min() - 1
+    max_lon = filtered_airports['longitude'].max() + 1
+    min_lat = filtered_airports['latitude'].min() - 1
+    max_lat = filtered_airports['latitude'].max() + 1
+
+    #tworzenie mapy
+    plt.figure(figsize=(12, 8))
+    m = Basemap(projection='merc', llcrnrlon=min_lon, urcrnrlon=max_lon, llcrnrlat=min_lat, urcrnrlat=max_lat, resolution='i')
+
+    m.drawcoastlines()
+    m.drawcountries()
+    m.drawstates()
+    m.drawmapboundary()
+
+    #punkty lotnisk
+    lons = filtered_airports['longitude'].tolist()
+    lats = filtered_airports['latitude'].tolist()
+    x, y = m(lons, lats)
+    m.scatter(x, y, marker='v', color='red', zorder = 5, s = 100)
+
+    #tytuł
+    plt.title(f'Możliwe kierunki w dniu {date_in_mm}')
+
+    #zapisuje mape do jpg
+    plt.savefig('static/mapa_lotnisk.jpg', format='jpg', dpi=300)
+
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 import DataAnalyzer as ds
 import matplotlib
-
+import MapMaker as mm
 matplotlib.use('agg')
 
 # --> Zrobić coś jak strona sie wypierdala! --> Tzn jakieś ładne "Ops, we're sorry"
@@ -27,11 +27,11 @@ def szukaj_lotow():
 
         if trip_type == 'false':
             flights = FS.azair_oneway(dep_airport, date_in)
-            #all_flights = FS.azair_oneway_multiple_days(dep_airport, date_in, 3)
-            #df = pd.DataFrame(all_flights) <-- Zakomentowane, zeby nie tworzyc ciagle nowej csv jak nie jest to konieczne
-            #df.to_csv('temp_logs/flights_data.csv', index=False)
-            ds.analyzer(date_in, mail_owner)
-
+            all_flights = FS.azair_oneway_multiple_days(dep_airport, date_in, 3)
+            df = pd.DataFrame(all_flights) #<-- Zakomentowane, zeby nie tworzyc ciagle nowej csv jak nie jest to konieczne
+            df.to_csv('temp_logs/flights_data.csv', index=False)
+            #ds.analyzer(date_in, mail_owner)
+            #mm.map_maker(date_in)
             return render_template('wyniki_oneway.html', dziennik_lotow=flights)
         elif trip_type == 'true':
             flights = FS.azair_return(dep_airport, date_in, date_out)
